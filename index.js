98ce789ec4a151a45788f795f4576452363a6bfa
agnescameron <agnesfcameron@gmail.com>
2020-04-19 17:40:32 -0400

    made basic structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+var router = express.Router();
+
+var app = express()
+
+app.use(express.static(__dirname + "/public"));
+
+// define the home page route
+router.get('/', function (req, res) {
+  res.redirect('index.html')
+})
+
+app.listen(3000, () => console.log('hello everyone!'));
+
---
+++ public/assets/js/lib/plants.js
@@ -0,0 +1,44 @@
+const plants = [
+
+	{
+		'name': 'mugwort',
+		'symbol': 'ӂ',
+		'color': '#009933',
+		'recipes': ['mugwort tea']
+	},
+
+	{
+		'name': 'japanese knotweed',
+		'symbol': 'Ѫ',
+		'color': '#009933',
+		'recipes': ['knotweed crumble']
+	},
+
+	{
+		'name': 'bitter dock',
+		'symbol': 'Գ',
+		'color': '#009933',
+		'recipes': ['hindbeh']
+	},
+
+	{
+		'name': 'wild garlic',
+		'symbol': '҂',
+		'color': '#009933',
+		'recipes': ['wild garlic pesto']
+	},
+
+	{
+		'name': 'dandelion greens',
+		'symbol': '෴',
+		'color': '#009933',
+		'recipes': ['hindbeh']
+	},
+
+	{
+		'name': 'nettles',
+		'symbol': 'ʬ',
+		'color': '#009933',
+		'recipes': ['nettle soup']
+	}
+]
