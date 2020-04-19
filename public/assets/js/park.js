d771661612018ee79f80afdb20ce414f924880b0
agnescameron <agnesfcameron@gmail.com>
2020-04-19 18:07:06 -0400

    layout colour and click event

--- public/assets/js/park.js
+++ public/assets/js/park.js
@@ -10,8 +10,12 @@ function growPlants() {
 		let plantDiv = $('<div/>', {
 			'class': `plant ${plant.name}`
 		})
+		.data('plant', plant)
 		.html(plant.symbol)
-		.css({'left': Math.random()*95 + '%', 'top': Math.random()*95 + '%'})
+		.css({'left': Math.random()*95 + '%', 'top': Math.random()*95 + '%', 'color': plant.color})
+		.click(function() {
+			console.log(`picked a ${$(this).data('plant').name}`)
+		})
 		$('#park').append(plantDiv)
 	}
 }
