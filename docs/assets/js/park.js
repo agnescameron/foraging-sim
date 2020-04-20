56ac505a131c597b4003498f1b05717348ca2eec
agnescameron <agnesfcameron@gmail.com>
2020-04-20 15:12:56 -0400

    wrote reduce method for array

--- docs/assets/js/park.js
+++ docs/assets/js/park.js
@@ -3,12 +3,32 @@ import { plants } from './lib/plants.js';
 // constants
 const numPlants = 100;
 
-//globals
+//evil global variable :o 
 let bag = [];
+
+//takes in an unsorted bag, creates a sorted
+//object with the amount of each plant type
+
+//QUESTION 3: replace for loops here with
+//arrow functions
+function countPlants(bag) {
+	return bag.reduce((totals, plant) => {
+		if(!totals[plant.name]) totals[plant.name] = {
+			name: plant.name,
+			total: 0
+		}
+		else totals[plant.name].total = totals[plant.name].total + 1;
+
+		return totals;
+	}, {})
+}
 
 function pickPlant(plantDiv) {
 	const plant = plantDiv.data('plant')
 	bag.push(plant)
+
+	const totals = countPlants(bag)
+	console.log(totals)
 
 	//append the item to the bag div
 	$('#bag-contents').append($('<div/>', {
